469e7bc5b13a8341f3ac05b6ad94424bdc4849c6
Nahuel7709 <nahuelmenichetti98@gmail.com>
2021-12-13 21:22:20 -0300

    primer commit mercado liebre

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require ("express")
+const app = express()
+const path = require ("path");
+
+
+app.listen(3000,()=>{console.log("Servidor Corriendo")});
+
+app.get("/",(req,res) =>{
+    let htmlPath = path.join (__dirname,"views/home.html");
+    res.sendFile(htmlPath);
+})
+
+app.get("/register",(req,res) =>{
+    res.sendFile (path.join(__dirname, "views/register.html"))
+})
+
+app.get("/login",(req,res) =>{
+    res.sendFile (path.join(__dirname, "views/login.html"))
+})
+
+
+app.use(express.static(path.resolve(__dirname, "public")));
